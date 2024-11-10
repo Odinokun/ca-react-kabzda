7191e68bf42ef2dfed30af125f33bfafeadb4795
odinokun <odinokun@gmail.com>
2024-11-10 10:08:45 -0500

    Refactor App component to remove unused Rating components and streamline Accordion usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { Accordion } from './components/Accordion/Accordion';
-import './App.css';
-import { Rating } from './components/Rating/Rating';
 import { FC } from 'react';
+import './App.css';
+import { Accordion } from './components/Accordion/Accordion';
 
 function App() {
   return (
     <div className='App'>
-      <PageTitle title='Rating 1' />
-      <Rating value={3} />
-      <hr />
-
       <Accordion titleValue='Menu' collapsed={true} />
-      <hr />
-
-      <PageTitle title='Rating 2' />
-      <Rating value={0} />
-      <Rating value={1} />
-      <Rating value={2} />
-      <Rating value={3} />
-      <Rating value={4} />
-      <Rating value={5} />
-      <hr />
-
       <Accordion titleValue='Users' collapsed={false} />
     </div>
   );
