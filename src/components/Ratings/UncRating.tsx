491f069491b883a681edf7a0ff75d60a5a4060e6
odinokun <odinokun@gmail.com>
2024-11-24 19:05:23 -0500

    Refactor UncRating and Star components for improved state management and interactivity

--- src/components/Ratings/UncRating.tsx
+++ src/components/Ratings/UncRating.tsx
@@ -1,3 +1,4 @@
+import { on } from 'events';
 import { FC, useState } from 'react';
 
 type PropsType = {};
@@ -5,35 +6,34 @@ type PropsType = {};
 export const UncRating: FC<PropsType> = () => {
   const [value, setValue] = useState<number>(0);
 
-  const onClickHandler = (value: number) => {
-    setValue(value);
-  };
-
   return (
-    <>
-      <div>
-        <button onClick={() => onClickHandler(1)}>1</button>
-        <button onClick={() => onClickHandler(2)}>2</button>
-        <button onClick={() => onClickHandler(3)}>3</button>
-        <button onClick={() => onClickHandler(4)}>4</button>
-        <button onClick={() => onClickHandler(5)}>5</button>
-        <button onClick={() => onClickHandler(0)}>Reset</button>
-      </div>
-      <div>
-        <Star selected={value > 0} />
-        <Star selected={value > 1} />
-        <Star selected={value > 2} />
-        <Star selected={value > 3} />
-        <Star selected={value > 4} />
-      </div>
-    </>
+    <div>
+      <Star selected={value > 0} value={1} setValue={setValue} />
+      <Star selected={value > 1} value={2} setValue={setValue} />
+      <Star selected={value > 2} value={3} setValue={setValue} />
+      <Star selected={value > 3} value={4} setValue={setValue} />
+      <Star selected={value > 4} value={5} setValue={setValue} />
+    </div>
   );
 };
 
 type StarPropsType = {
   selected: boolean;
+  value: number;
+  setValue: (value: number) => void;
 };
 
-const Star: FC<StarPropsType> = ({ selected }) => {
-  return <span>{selected ? <b>star </b> : 'star '}</span>;
+const Star: FC<StarPropsType> = ({
+  selected,
+  value,
+  setValue,
+  ...restProps
+}) => {
+  const onClickHandler = () => setValue(value);
+
+  return (
+    <span style={{ cursor: 'pointer' }} onClick={onClickHandler}>
+      {selected ? <b>star </b> : 'star '}
+    </span>
+  );
 };
